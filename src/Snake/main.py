649175d3a8de0618261bff9760d6e908eeb31a8d
novenopatch <novenosexta77@gmail.com>
2022-08-02 23:00:34 +0000

    basic founctional snake

--- src/Snake/main.py
+++ src/Snake/main.py
@@ -8,6 +8,7 @@ class Snake:
         self.LEFT = "left"
         self.body = [pygame.math.Vector2(5,10),pygame.math.Vector2(6,10),pygame.math.Vector2(7,10)]
         self.direction =  pygame.math.Vector2(1,0)
+        self.newBlock = False
 
     def drawSnake(self):
         for block in self.body:
@@ -16,9 +17,18 @@ class Snake:
             blockRect = pygame.Rect(posX,posY,cellSize,cellSize)
             pygame.draw.rect(screen, (183, 111, 122), blockRect)
     def moveSnake(self):
-        bodyCopy = self.body[:-1]
-        bodyCopy.insert(0,bodyCopy[0] + self.direction)
-        self.body = bodyCopy[:]
+        if self.newBlock == True:
+            bodyCopy = self.body[:]
+            bodyCopy.insert(0, bodyCopy[0] + self.direction)
+            self.body = bodyCopy[:]
+            self.newBlock = False
+        else:
+            bodyCopy = self.body[:-1]
+            bodyCopy.insert(0,bodyCopy[0] + self.direction)
+            self.body = bodyCopy[:]
+
+    def adBlock(self):
+        self.newBlock = True
     def changeDirection(self,direction:str):
         if direction == self.UP:
             self.direction = pygame.math.Vector2(0, -1)
@@ -29,18 +39,18 @@ class Snake:
         if direction == self.LEFT:
             self.direction = pygame.math.Vector2(-1, 0)
 
-        #elif direction == "down"
-
-
 class Fruit:
     def __init__(self):
-        self.x = random.randint(0,cellNumber -1)
-        self.y = random.randint(0,cellNumber -1)
-        self.pos = pygame.math.Vector2(self.x,self.y)
+        self.randomize()
+    def randomize(self):
+        self.x = random.randint(0, cellNumber - 1)
+        self.y = random.randint(0, cellNumber - 1)
+        self.pos = pygame.math.Vector2(self.x, self.y)
     def drawFruit(self):
         #create rectable
         fruitRect = pygame.Rect(int(self.pos.x * cellSize),int(self.pos.y * cellSize),cellSize,cellSize)
         pygame.draw.rect(screen,(126,166,144),fruitRect)
+
 
 class Main:
     def __init__(self):
@@ -49,11 +59,14 @@ class Main:
     def update(self):
         self.snake.moveSnake()
     def drawElements(self):
-        mainGame.snake.drawSnake()
-        mainGame.fruit.drawFruit()
+        self.snake.drawSnake()
+        self.fruit.drawFruit()
     def checkCollision(self):
         if self.fruit.pos == self.snake.body[0]:
-            print("yo")
+            self.fruit.randomize()
+            self.snake.adBlock()
+            #self.snake.moveSnake()
+
 pygame.init()
 cellSize = 40
 cellNumber = 20
